179ee25ea2a8f58ec9f57350e89d8e70712acd0c
jsruinedmylife <zychmateusz11@gmail.com>
2018-03-05 11:25:35 +0100

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
+
+export class loginComponent {
+    enter_login: string = 'Your login';
+    enter_pass: string = 'Your password';
+    forgetUrl: string = ''; //link do reset password
+    rememberMe: string = 'Remember me';
+    submit: string = 'Log In';
+    login: string;
+    pass: string;
+
+    onSubmit() {
+
+    }
+}
