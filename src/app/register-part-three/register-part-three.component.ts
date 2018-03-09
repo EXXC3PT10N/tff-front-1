4a465a7570058acd762c4bd0b927e4dcea44f6d4
msz64 <msz64@wp.pl>
2018-03-09 19:01:51 +0100

    elo

--- src/app/register-part-three/register-part-three.component.ts
+++ src/app/register-part-three/register-part-three.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-part-three',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPartThreeComponent implements OnInit {
 
-  constructor() { }
+  head_message: string = "Rejestracja przebiegła pomyślnie!";
+  butt_message: string = "Zakończ";
+  
+
+  constructor(private _router: Router) { }
 
   ngOnInit() {
   }
