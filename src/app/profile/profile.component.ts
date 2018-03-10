2ae36afa4e6b3864084df718b85b3062d7e76a8e
msz64 <msz64@wp.pl>
2018-03-10 14:22:48 +0100

    elo

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -21,6 +21,18 @@ export class ProfileComponent implements OnInit{
   _langFilter: string;
   filteredLanguages: string[];
   userLanguages;
+  specializations: string[];
+  _specFilter: string;
+  filteredSpecializations: string[];
+  userSpec;
+  software: string[];
+  _softwareFilter: string;
+  filteredSoftware: string[];
+  userSoftware;
+  certifications: string[];
+  _certificationsFilter: string;
+  filteredCertifications: string[];
+  userCertifications;
   
   constructor(private _profileService: ProfileService, private _authService: AuthService, private _router: Router){}
 
@@ -33,12 +45,14 @@ export class ProfileComponent implements OnInit{
       this._profileService.getLanguagesNames().subscribe(languages => this.languages = languages);
       this._profileService.getUserLanguages().subscribe(userLanguages => { 
         this.userLanguages = userLanguages.employee.languages;
-        console.log(JSON.stringify(this.userLanguages))});
-  }
-
-
-  logout() {
-    this._authService.logout();
+        });
+      this._profileService.getSpecializationsNames().subscribe(specializations => this.specializations = specializations)
+      this._profileService.getUserSpec().subscribe(userSpec => this.userSpec = userSpec.employee.specs);
+      this._profileService.getSoftwareNames().subscribe(software => this.software = software);
+      this._profileService.getUserSoftware().subscribe(userSoftware => this.userSoftware = userSoftware.employee.software);
+      this._profileService.getCertificationsNames().subscribe(certifications => this.certifications = certifications);
+      this._profileService.getUserCertifications().subscribe(userCertifications => this.userCertifications = userCertifications.employee.certifications);
+      
   }
 
   get langFilter(): string {
@@ -47,17 +61,50 @@ export class ProfileComponent implements OnInit{
 
 set langFilter(value: string) {
   this._langFilter = value;
- // this.filteredLanguages = this.langFilter ? this.performFilter(this.langFilter) : this.languages;
-  this.filteredLanguages = this.langFilter ? this.performFilter(this.langFilter) : null;
+  this.filteredLanguages = this.langFilter ? this.performFilter(this.langFilter, this.languages) : null;
 }
 
-performFilter(filterBy: string): string[] {
+get specFilter(): string {
+  return this._specFilter;
+}
+
+set specFilter(value: string) {
+this._specFilter = value;
+this.filteredSpecializations = this.specFilter ? this.performFilter(this.specFilter, this.specializations) : null;
+}
+
+get softwareFilter(): string {
+  return this._softwareFilter;
+}
+
+set softwareFilter(value: string) {
+this._softwareFilter = value;
+this.filteredSoftware = this.softwareFilter ? this.performFilter(this.softwareFilter, this.software) : null;
+}
+
+get certificationsFilter(): string {
+  return this._certificationsFilter;
+}
+
+set certificationsFilter(value: string) {
+this._certificationsFilter = value;
+this.filteredCertifications = this.certificationsFilter ? this.performFilter(this.certificationsFilter, this.certifications) : null;
+}
+
+
+  logout() {
+    this._authService.logout();
+  }
+
+  
+
+performFilter(filterBy: string, variable: string[]): string[] {
   filterBy = filterBy.toLocaleLowerCase();
-  return this.languages.filter((language: string) =>
-        language['name'].toLocaleLowerCase().indexOf(filterBy) !== -1);
+  return variable.filter((F: string) =>
+        F['name'].toLocaleLowerCase().indexOf(filterBy) !== -1);
 }
 
-confirm(lang): void{
+confirmLang(lang): void{
   
   this._profileService.setLanguages(lang).subscribe()
   
@@ -73,7 +120,7 @@ addLang(name: string): void{
   lang.languages.push(name);
   let tmp = { "name": lang.languages[lang.languages.length-1] }
   this.userLanguages.push(tmp);
-  this.confirm(lang);
+  this.confirmLang(lang);
 }
 
 deleteLang(name: string){
@@ -86,8 +133,115 @@ deleteLang(name: string){
   let lang = {"languages": new Array()};
   for(let userLanguage of this.userLanguages)
     lang.languages.push(userLanguage.name);
-  this.confirm(lang);
+  this.confirmLang(lang);
   // console.log("Indeks: "+pos);
 }
+
+confirmSpec(spec): void{
+  
+  this._profileService.setSpecializations(spec).subscribe()
+  
+  //console.log(JSON.stringify(lang));
+  // this._profileService.setLanguages("").subscribe()
+  
+  //console.log(JSON.stringify(this.userLanguages));
+}
+addSpec(name: string): void{
+  let nowy = {"specs": new Array()};
+  for(let userSpec of this.userSpec)
+    nowy.specs.push(userSpec.name);
+  nowy.specs.push(name);
+  console.log(JSON.stringify(nowy));
+  let tmp = { "name": nowy.specs[nowy.specs.length-1] }
+  this.userSpec.push(tmp);
+  this.confirmSpec(nowy);
+}
+
+deleteSpec(name: string){
+  // console.log("nazwa: "+name);
+  // console.log(JSON.stringify(this.userLanguages));
+  let obj = {"name": name};
+  let pos = this.userSpec.map(function(e) { return e.name; }).indexOf(name);
+  
+  if(pos > -1)
+    this.userSpec.splice(pos, 1);
+  let spec = {"specs": new Array()};
+  for(let userSpec of this.userSpec)
+    spec.specs.push(userSpec.name);
+  this.confirmSpec(spec);
+  // console.log("Indeks: "+pos);
+}
+
+confirmSoftware(software): void{
+  
+  this._profileService.setSoftware(software).subscribe()
+  
+  //console.log(JSON.stringify(lang));
+  // this._profileService.setLanguages("").subscribe()
+  
+  //console.log(JSON.stringify(this.userLanguages));
+}
+addSoftware(name: string): void{
+  let nowy = {"software": new Array()};
+  for(let userSoft of this.userSoftware)
+    nowy.software.push(userSoft.name);
+  nowy.software.push(name);
+  console.log(JSON.stringify(nowy));
+  let tmp = { "name": nowy.software[nowy.software.length-1] }
+  this.userSoftware.push(tmp);
+  this.confirmSoftware(nowy);
+}
+
+deleteSoftware(name: string){
+  // console.log("nazwa: "+name);
+  // console.log(JSON.stringify(this.userLanguages));
+  let obj = {"name": name};
+  let pos = this.userSoftware.map(function(e) { return e.name; }).indexOf(name);
+  
+  if(pos > -1)
+    this.userSoftware.splice(pos, 1);
+  let soft = {"software": new Array()};
+  for(let userSoft of this.userSoftware)
+    soft.software.push(userSoft.name);
+  this.confirmSoftware(soft);
+  // console.log("Indeks: "+pos);
+}
+
+confirmCertifications(certifications): void{
+  
+  this._profileService.setCertifications(certifications).subscribe()
+  
+  //console.log(JSON.stringify(lang));
+  // this._profileService.setLanguages("").subscribe()
+  
+  //console.log(JSON.stringify(this.userLanguages));
+}
+addCertifications(name: string): void{
+  let nowy = {"certifications": new Array()};
+  for(let userCert of this.userCertifications)
+    nowy.certifications.push(userCert.name);
+  nowy.certifications.push(name);
+  console.log(JSON.stringify(nowy));
+  let tmp = { "name": nowy.certifications[nowy.certifications.length-1] }
+  this.userCertifications.push(tmp);
+  this.confirmCertifications(nowy);
+}
+
+deleteCertifications(name: string){
+  // console.log("nazwa: "+name);
+  // console.log(JSON.stringify(this.userLanguages));
+  let obj = {"name": name};
+  let pos = this.userCertifications.map(function(e) { return e.name; }).indexOf(name);
+  
+  if(pos > -1)
+    this.userCertifications.splice(pos, 1);
+  let cert = {"certifications": new Array()};
+  for(let userCert of this.userCertifications)
+    cert.certifications.push(userCert.name);
+  this.confirmCertifications(cert);
+  // console.log("Indeks: "+pos);
+}
+
+
 
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -12,6 +12,7 @@ export class ProfileService {
   envPath: string = environment.path;
   idUrl: string = this.envPath + "/api/auth/id";
   identityUrl: string = this.envPath + '/api/user/me';
+  skillsUrl: string = this.envPath + '/api/skills';
   updateUrl: string = this.envPath + '/api/employee/update';
 
   constructor(private _http: HttpClient) { }
@@ -25,7 +26,12 @@ export class ProfileService {
   }
 
   getLanguagesNames(): Observable<string[]>{
-    let url: string = environment.path + "/api/skills/languages";
+    let url: string = this.skillsUrl + "/languages";
+    return this._http.get<string[]>(url);
+  }
+
+  getSpecializationsNames(): Observable<string[]>{
+    let url: string = this.skillsUrl + "/specializations";
     return this._http.get<string[]>(url);
   }
 
@@ -38,6 +44,51 @@ export class ProfileService {
     let url = this.updateUrl + "/languages";
 
     return this._http.post<any>(url, language);
+  }
+  
+  setSpecializations(specialization): Observable<any>{
+    let url = this.updateUrl + "/specs";
+
+    return this._http.post<any>(url, specialization);
+  }
+
+  getUserSpec(): Observable<any>{
+    return this._http.get<any>(this.identityUrl)
+    //.do(data => console.log(JSON.stringify(data)));
+  }
+
+  getSoftwareNames(): Observable<string[]>{
+    let url: string = this.skillsUrl + "/software";
+    return this._http.get<string[]>(url);
+  }
+
+  getUserSoftware(): Observable<any>{
+    return this._http.get<any>(this.identityUrl)
+    //.do(data => console.log(JSON.stringify(data)));
+  }
+
+
+  setSoftware(software): Observable<any>{
+    let url = this.updateUrl + "/software";
+
+    return this._http.post<any>(url, software);
+  }
+
+  getCertificationsNames(): Observable<string[]>{
+    let url: string = this.skillsUrl + "/certifications";
+    return this._http.get<string[]>(url);
+  }
+
+  getUserCertifications(): Observable<any>{
+    return this._http.get<any>(this.identityUrl)
+    //.do(data => console.log(JSON.stringify(data)));
+  }
+
+
+  setCertifications(certifications): Observable<any>{
+    let url = this.updateUrl + "/certifications";
+
+    return this._http.post<any>(url, certifications);
   }
 
 }
