85fa929d685439f8ee2d3cdcbdf2d57e39e4017e
msz64 <msz64@wp.pl>
2018-03-20 00:26:32 +0100

    image-test work

--- src/app/image-test/image-test.component.ts
+++ src/app/image-test/image-test.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../services/auth.service';
 import { RequestOptions, Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { ProfileService } from '../services/profile.service';
 
 
 
@@ -39,15 +40,28 @@ export class ImageTestComponent implements OnInit {
   //   this.hasAnotherDropZoneOver = e;
   // }
   
+  image: any;
+  url: string = 'http://localhost:3000/user/';
   apiEndPoint: string = "http://localhost:3000/api/user/image/upload"
+  warunek: boolean = false;
   
-  constructor(private _authService: AuthService, private _http: HttpClient) { 
+  constructor(private _authService: AuthService, private _http: HttpClient, private _profileService: ProfileService) { 
     // this.token = this.getToken();
     // console.log("token: "+this.token)
   }
 
 
   ngOnInit() {
+    // this._profileService.getIdentity().subscribe(data => this._profileService.getImage(data.user.image).subscribe(dane => {
+    
+    //   console.log("Dane"+ dane)
+    // }))
+    this._profileService.getIdentity().subscribe(data => {
+      this.url += data.user.image
+      this.warunek = true;
+    })
+    
+    
   }
 
   fileChange(event) {
