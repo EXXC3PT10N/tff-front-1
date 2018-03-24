4f59289c7492d64f2b42cbcf5d62552b16bc3321
Mati <zychmateusz11@gmail.com>
2018-03-24 14:43:28 +0100

    rates

---
+++ src/app/rate/rate.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: './rate.component.html'
+})
+export class RateComponent implements OnInit {
+
+
+
+
+  constructor(private _router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
