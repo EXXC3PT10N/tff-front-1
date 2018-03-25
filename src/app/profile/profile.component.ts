81fc9da5fbfab5e55efd9647431b30a4e97b3bc5
msz64 <msz64@wp.pl>
2018-03-25 21:58:23 +0200

    some improvments in profile and stranger profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
 import { UserProfile } from './userProfile';
 import { AuthService } from "../services/auth.service";
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FullUser, Company } from '../models/fullUser';
 import { DialogProfileComponent } from '../dialog-profile/dialog-profile.component';
 import { MatDialog } from '@angular/material';
@@ -63,6 +63,8 @@ export class ProfileComponent implements OnInit{
               private _firebaseMessage: FirebaseMessagingService){}
 
   ngOnInit(): void {
+      
+      
       this._profileService.getIdentity().subscribe(userProfile => {
         this.user = userProfile;
         this.imie = userProfile["user"].first_name;
