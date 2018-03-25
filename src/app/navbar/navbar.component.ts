330b05a5660f697f0285464c5a49302890e42c16
Nikodem Strawa <nikodem@Mac.lan>
2018-03-25 23:33:39 +0200

    Navbar message on push notification

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseMessagingService} from '../services/firebase.messaging.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './navbar.component.html',
   styleUrls: ['../../assets/assets_bid/css/Navigation-Clean.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  hasNewMessages: boolean;
 
+  constructor(private _firebaseMessage: FirebaseMessagingService){}
 
-
+  ngOnInit(): void {
+    this._firebaseMessage.getMessagingFunction().onMessage(data => {
+      this.hasNewMessages = true;
+    });
+  }
 }
